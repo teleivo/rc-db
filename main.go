e57d41c02eb584012762a4676f9b338cdb0fc5cc
teleivo <teleivo@users.noreply.github.com>
2025-11-10 17:26:57 +0100

    fix: /get?a=b error message

--- main.go
+++ main.go
@@ -16,7 +16,6 @@ func main() {
 	}
 }
 
-// TODO slog listening and requests
 // TODO add tests on the HTTP or DB layer? (also that set?a= deletes the value)
 // TODO encapsulate state so we can then refactor it to write to a file
 // TODO should I allow setting it to nothing? or rather expect a DELETE
@@ -27,7 +26,7 @@ func run(w io.Writer) error {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/set", func(w http.ResponseWriter, r *http.Request) {
 		query := r.URL.Query()
-		logger.Info("/set", query)
+		logger.Info("/set", slog.Any("query", query))
 		if len(query) != 1 {
 			w.WriteHeader(http.StatusBadRequest)
 			fmt.Fprintf(w, "must set exactly one key using /set?somekey=somevalue")
@@ -40,29 +39,24 @@ func run(w io.Writer) error {
 			return
 		}
 
-		fmt.Println(k, values)
 		dbState[k] = values[0]
 	})
 	mux.HandleFunc("/get", func(w http.ResponseWriter, r *http.Request) {
 		query := r.URL.Query()
-		logger.Info("/get", query)
+		logger.Info("/get", slog.Any("query", query))
 		if len(query) != 1 {
 			w.WriteHeader(http.StatusBadRequest)
 			fmt.Fprintf(w, "must get exactly one key using /get?somekey")
 			return
 		}
 		k, values := getKeyValue(query)
-		fmt.Println(query.Has(k))
-		fmt.Println(k, values)
-		// TODO len(values)=1 with empty string on get?a
-		// if len(values) > 0 {
-		// 	w.WriteHeader(http.StatusBadRequest)
-		// 	fmt.Fprintf(w, "get must be called without a value, did you mean to set using /set?somekey=somevalue")
-		// 	return
-		// }
+		if len(values) > 0 && values[0] != "" {
+			w.WriteHeader(http.StatusBadRequest)
+			fmt.Fprintf(w, "get must be called without a value, did you mean to set using /set?somekey=somevalue")
+			return
+		}
 
 		v, ok := dbState[k]
-		fmt.Println(v, ok)
 
 		if !ok {
 			w.WriteHeader(http.StatusNotFound)
@@ -74,6 +68,7 @@ func run(w io.Writer) error {
 		Addr:    "127.0.0.1:4000",
 		Handler: mux,
 	}
+	logger.Info("Listening", "Addr", srv.Addr)
 	return srv.ListenAndServe()
 }
 
