598a26296deff1309dd14f0f70a74fbe83c75246
teleivo <teleivo@users.noreply.github.com>
2025-11-22 05:00:30 +0100

    fix: add mutex

--- db.go
+++ db.go
@@ -1,7 +1,10 @@
 // Package db provides a key-value store.
 package db
 
+import "sync"
+
 type DB struct {
+	mu    sync.RWMutex
 	state map[string]string
 }
 
@@ -10,10 +13,14 @@ func New() *DB {
 }
 
 func (d *DB) Set(k, v string) {
+	d.mu.Lock()
+	defer d.mu.Unlock()
 	d.state[k] = v
 }
 
 func (d *DB) Get(k string) (string, bool) {
+	d.mu.RLock()
+	defer d.mu.RUnlock()
 	v, ok := d.state[k]
 	return v, ok
 }
